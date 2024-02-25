ad43c6e1d12994bedba4712b5d8ebc7612028121
numtel <ben@latenightsketches.com>
2024-02-24 21:16:12 -0800

    Revert "Footer blockquote"
    This reverts commit 9848108706ff4e5c29f36c993286c60093a29f91.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
   useAccount,
 } from 'wagmi';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faEthereum } from '@fortawesome/free-brands-svg-icons';
 
 import { chainContracts } from './contracts.js';
@@ -37,15 +38,12 @@ export default function App() {
       </>}
 
       <footer>
-        <blockquote>Show me what democracy looks like!</blockquote>
-        <div className="icons">
-          <a href="https://github.com/numtel/democoin" rel="noopener" target="_blank" title="Github Repository">
-            <FontAwesomeIcon icon={faGithub} size="2xl" />
-          </a>&nbsp;
-          <a href={contracts.explorer + 'address/' + contracts.DemoERC721.address} rel="noopener" target="_blank" title="Collection on Block Explorer">
-            <FontAwesomeIcon icon={faEthereum} size="2xl" />
-          </a>
-        </div>
+        <a href="https://github.com/numtel/democoin" rel="noopener" target="_blank" title="Github Repository">
+          <FontAwesomeIcon icon={faGithub} size="2xl" />
+        </a>&nbsp;
+        <a href={contracts.explorer + 'address/' + contracts.DemoERC721.address} rel="noopener" target="_blank" title="Collection on Block Explorer">
+          <FontAwesomeIcon icon={faEthereum} size="2xl" />
+        </a>
       </footer>
     </main>
   );
